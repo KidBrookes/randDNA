822d6970f732cccb1ff19e2ab700d57dc2472ba2
KidBrookes <username@users.reply.github.com>
2019-03-14 17:25:21 -0400

    Added source file

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -4,18 +4,22 @@
 using namespace std;
 
 string randDNA(int seed, string bases, int n){
-string letters ="";
+string letter ="";
 int min =0;
-int max = bases.size()-1;
-int index = -1;
+int max =bases.size() -1;
 
-mt19937 mt(seed);
-uniform_int_distribution<> range(min, max);
 
-for(int i= 0; i< n; i++){
-cout<<range(mt);
-index =range(mt);
-letters +=bases[index];
+mt19937 dna1(seed);
+uniform_int_distribution<int> range(min,max);
+
+if(bases == ""){
+return "";
 }
-return letters;
+
+for(int i=0; i<n;i++){
+ 
+letter += bases[range(dna1)];
+
+}
+return letter;
 }
